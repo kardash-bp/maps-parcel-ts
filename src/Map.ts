a75d70b5efddb9f197172da1d46fb7d15c3558f9
kardash-bp <kardasbp@gmail.com>
2021-04-17 11:12:56 +0200

    map marker

--- src/Map.ts
+++ src/Map.ts
@@ -1,12 +1,29 @@
+interface Mappable {
+  location: { lat: number; lng: number }
+}
+
 export class Map {
   private googleMap: google.maps.Map
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 5,
+      zoom: 1,
       center: {
         lat: 0,
         lng: 0,
       },
+    })
+  }
+  addMarker(obj: Mappable) {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: obj.location.lat,
+        lng: obj.location.lng,
+      },
+    })
+    const infoWindow = new google.maps.InfoWindow({ content: 'Hello!' })
+    marker.addListener('click', () => {
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-// import { Company } from './Company'
-// import { User } from './User'
+import { Company } from './Company'
+import { User } from './User'
 import { Map } from './Map'
 
 const map = new Map('map')
+const user = new User()
+const comp = new Company()
+
+map.addMarker(user)
+map.addMarker(comp)
